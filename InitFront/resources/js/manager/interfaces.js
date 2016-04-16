8d0fe7e685e166e407b70dde296a86e4372d4903
kapta <chanzig@qq.com>
2016-04-16 19:20:49 +0800

    update

---
+++ InitFront/resources/js/manager/interfaces.js
@@ -0,0 +1,12 @@
+define(function(require, exports, module) {
+    var interfaces={
+        //index
+        "userOpenId":"pub/getOpenId.json",
+         'TestInder':'http://gc.ditu.aliyun.com/geocoding',
+         
+         //aboutus
+         'aboutus':'pub/aboutus.do',
+    }
+     
+    module.exports = interfaces;
+});
